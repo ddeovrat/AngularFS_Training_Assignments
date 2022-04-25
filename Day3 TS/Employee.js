a8adaf3bd05a9c4409ab7ac53b02ffe917e7b04c
Deepankar <deepankardeovrat@gmail.com>
2022-04-25 14:20:11 +0530

    Add files via upload
    Adding Typescript first assignment

---
+++ Day3 TS/Employee.js
@@ -0,0 +1,7 @@
+var empId = 5464654;
+var empName = "Scott";
+var empJob = "Manager";
+var isPermanentEmployee = true;
+var empSalary = 45000;
+var empDeptNo = 10;
+console.log("Employee Id: ".concat(empId, "\nEmployee Name: ").concat(empName, "\nEmployee Job: ").concat(empJob, "\nIs Permanent Employee: ").concat(isPermanentEmployee, "\nEmployee Salary: INR. ").concat(empSalary, "\nEmployee Deptno: ").concat(empDeptNo, "\n"));
---
+++ Day3 TS/Employee.ts
@@ -0,0 +1,8 @@
+let empId:number = 5464654;
+let empName:string = "Scott";
+let empJob:string = "Manager";
+let isPermanentEmployee:boolean = true;
+let empSalary:number = 45000;
+let empDeptNo:number = 10;
+
+console.log(`Employee Id: ${empId}\nEmployee Name: ${empName}\nEmployee Job: ${empJob}\nIs Permanent Employee: ${isPermanentEmployee}\nEmployee Salary: INR. ${empSalary}\nEmployee Deptno: ${empDeptNo}\n`);

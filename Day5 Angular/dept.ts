4322d213f12818d86b76da0044fafcaa8c76f24d
Deepankar <deepankardeovrat@gmail.com>
2022-04-27 16:47:43 +0530

    Add files via upload

---
+++ Day5 Angular/dept.ts
@@ -0,0 +1,14 @@
+export class Dept{
+    deptno:number=0;
+    dname:string="";
+    loc:string="";
+  }
+
+  export class Prod{
+    pid:number=0;
+    pname:string="";
+    price:number=0;
+    category:string="";
+    qty:number=0;
+  }
+
